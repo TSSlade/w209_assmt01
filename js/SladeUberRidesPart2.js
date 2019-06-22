a0835be8def54fdf8749ce5d84c2aa8d3dc6c299
Tim Slade <tslade@ischool.berkeley.edu>
2019-06-22 10:36:17 +0300

    Corrected issue with mismapped weekday/date

--- js/SladeUberRidesPart2.js
+++ js/SladeUberRidesPart2.js
@@ -1,5 +1,12 @@
 // Creating library to hold various visualization functions
 
+var div = d3
+            // .select("body")
+            .select("div#viz")
+            .append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
 var UberRidesVizLib = UberRidesVizLib || {};
 
 // Creating ScatterPlot viz function
@@ -9,30 +16,30 @@ UberRidesVizLib.ScatterPlot = function() {
     // Parameters to define size of canvas, figure
     var screenWidth = 800,
         screenHeight = 600,
-        margin = {"top":  0.025 * screenHeight,
-                  "bottom": 0.025 * screenHeight,
-                  "left": 0.025 * screenWidth,
-                  "right": 0.025 * screenWidth},
-        canvasWidth = screenWidth - margin.left - margin.right, // 800 - 80 - 80 = 640
-        canvasHeight = screenHeight - margin.top - margin.bottom;
+        margin = {"top":  0.05 * screenHeight,    // 40 px
+                  "bottom": 0.1 * screenHeight,  // 40 px
+                  "left": 0.05 * screenWidth,     // 30 px
+                  "right": 0.05 * screenWidth},   // 30 px
+        canvasWidth = (screenWidth - margin.left - margin.right),   // 800 - 40 - 40 = 720
+        canvasHeight = (screenHeight - margin.top - margin.bottom); // 600 - 30 - 30 = 540
 
     // Selecting the proper div
     var svg = d3.select("div#viz")
                 .append("svg")
-                .attr("width", canvasWidth)
-                .attr("height", canvasHeight);
+                .attr("width", screenWidth)
+                .attr("height", screenHeight);
 
     // Setting up X scaler, mapper, and value obtainer
 
     // Given a datum, extract the weekday
     var xValue = function(d, i) {
-        console.log("weekday = " + d);
+        console.log("weekday = " + d.weekday);
         return d.weekday;};
 
     // Define scaler to map to the canvasWidth; domain comes later
     var xScale = d3.scalePoint()
-                   // .domain(weekdays)
                    .range([0, canvasWidth])
+                   // .range([1, 7])
                    .padding(10);
 
     // Given a datum, return the scaled value of the weekday
@@ -50,8 +57,6 @@ UberRidesVizLib.ScatterPlot = function() {
     var yValue = function(d, i) {
                       console.log("yValue > parseTimestamp:");
                       console.log(d);
-                      // console.log(d[0]);
-                      // console.log(d[1]);
                       var hour = parseTimestamp(d.timestamp_adj).getHours();
                       var minute = parseTimestamp(d.timestamp_adj).getMinutes();
                       // second = parseTimestamp(d.timestamp_adj).getSeconds();
@@ -61,10 +66,7 @@ UberRidesVizLib.ScatterPlot = function() {
                       return parseTimeOnly(`${hour}:${minute}`);
                   };
     // Define scaler to map to canvasHeight; domain comes later
-    // yScale = d3.scaleLinear().domain([0, 24]).range([canvasHeight, 0]),
     var yScale = d3.scaleTime()
-                   // .domain([new Date("January 1 1900 00:00:00"),
-                   //      new Date("January 2 1900 00:00:00")])
                    .range([canvasHeight, 0]);
 
     // Given a datum, return the scaled value of the timestamp
@@ -74,7 +76,8 @@ UberRidesVizLib.ScatterPlot = function() {
                 };
 
     // Define the yAxis
-    var yAxis = d3.axisLeft(yScale);
+    var yAxis = d3.axisLeft(yScale)
+                  .tickFormat(d3.timeFormat("%H:%M"));
 
     // Create a placeholder for the callback function
     var callback = function() {};
@@ -110,66 +113,74 @@ UberRidesVizLib.ScatterPlot = function() {
                     .data(data)
                     .enter()
                     .append("circle")
-                    .attr("r", "2px")
+                    .attr("id", "data_points")
+                    .attr("r", "4px")
                     .attr('cx', function(d, i) { return xMap(d);})
                     .attr('cy', function(d, i) { return yMap(d);})
+                    .attr("fill", function(d) {
+                              return make_colors(make_dict[d.vehicle_make]);
+                            })
                     .attr('transform',
-                          'translate(' + margin.left + ',' + margin.top + ")")
-                    .on("mouseover", function(d,i) {
-                                          console.log("make: " + d.vehicle_make);
-                                          deemphasize(d);
-                                          highlight(d);
-                                        })
-                    .on("mouseout", function(d, i) {
-                                          restore(d);})
-                    .on("click", function(d) {
-                                      console.log("Clicked on " + d.vehicle_make);
+                          'translate(' + 2.5 * margin.left + ',' + margin.top + ")")
+                    .on("mouseover", function(d) {
+                                      console.log("Hovering over " + d.vehicle_make);
                                       div.transition()
                                          .duration(200)
                                          .style("opacity", 0.9);
-                                      div.html("Make: " + d.vehicle_make + "<br/>" +
-                                               "Model: " + d.vehicle_model + "<br/>" +
-                                               "Date: " + d.date_year +
+                                      div.html("<em><strong>Make:</strong></em> " + d.vehicle_make + "<br/>" +
+                                               "<em><strong>Model:</strong></em> " + d.vehicle_model + "<br/>" +
+                                               "<em><strong>Date:</strong></em> " + d.date_year +
                                                "/" + d.date_month +
                                                "/" + d.date_day)
                                          .style("left", (d3.event.pageX + 10) + "px")
-                                         .style("top", (d3.event.pageY - 28) + "px");});
-
+                                         .style("top", (d3.event.pageY - 28) + "px");
+                                         })
+                    .on("mouseout", function(d, i) {
+                                          restore(d);
+                                          div.transition()
+                                             .duration(200)
+                                             .style("opacity", 0);})
+                    .on("click", function(d,i) {
+                                          console.log("make: " + d.vehicle_make);
+                                          console.log("time: " + d.timestamp_adj);
+                                          deemphasize(d);
+                                          highlight(d);
+                                        });
         // Generate the x axis
         svg.append("g")
            .attr("class", "x axis")
            .attr("transform",
-                 'translate(' + margin.left + ',' + canvasHeight + ")")
+                 'translate(' + 2.5 * margin.left + ',' + (canvasHeight + margin.top) + ")")
            .call(xAxis);
 
         // Create the x axis label
         svg.append("text")
            .attr("transform",
-                 "translate(" + (canvasWidth/2) + "," +               // Centering L to R
-                                (canvasHeight + margin.top/2) + ")")  // Aligning with bottom
+                 "translate(" + (2.5 * margin.left + canvasWidth/2) + "," +               // Centering L to R
+                                (canvasHeight + 2 * margin.top) + ")")  // Aligning with bottom
            .style("text-anchor", "middle")
-           .text("Weekday");
+           .text("Weekday")
+           .attr("font-weight", 700);
 
         svg.append("g")
            .attr("class", "y axis")
-           .attr("transform", 'translate(' + margin.left + ',' + margin.top + ")")
+           .attr("transform", 'translate(' + 2.5 * margin.left + ',' + margin.top + ")")
            .call(yAxis);
 
         // text label for the y axis
         svg.append("text")
-           .attr("transform", "rotate(-90)")
-           .attr("y", 0 - margin.left)
-           .attr("x", 0 - (canvasHeight / 2))
+           .attr("transform", "translate(" + 0.5 * margin.left + "," + canvasHeight/2 + ") rotate(-90)")
            .attr("dy", "1em")
            .style("text-anchor", "middle")
-           .text("Hour of the Day");
+           .text("Hour of the Day")
+           .attr("font-weight", 700);
     };
 
     var highlight = function(point) {
                       console.log("highlighting: " + point);
                       circle.filter(function(d, i) {
                         return (d.vehicle_make === point.vehicle_make); })
-                        .attr("r", "10px")
+                        .attr("r", "8px")
                         .attr("fill", function(d) {
                               return make_colors(make_dict[d.vehicle_make]);
                             })};
@@ -178,19 +189,15 @@ UberRidesVizLib.ScatterPlot = function() {
                       console.log("deemphasizing non-: " + point);
                       circle.filter(function(d, i) {
                         return (d === point) ? false : true; })
-                            .attr("r", "1px")
+                            .attr("r", "2.5px")
                             .attr("fill", "grey")};
 
     var restore = function(point) {
                       console.log("highlighting: " + point);
-                      circle.attr("r", "2px")
-                            .attr("fill", "black")};
-
-    var show_metadata = function(point) {
-                      console.log("showing metadata for: " + point);
-                      return 
-    }
-
+                      circle.attr("r", "4px")
+                            .attr("fill", function(d) {
+                              return make_colors(make_dict[d.vehicle_make]);
+                            })};
 
     var public = {
       "plot": plot_,
@@ -227,10 +234,6 @@ var make_dict = {
 
 var parseTimestamp = d3.timeParse("%Y-%m-%Y %I:%M");
 var parseTimeOnly = d3.timeParse("%I:%M");
-
-var div = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
 
 // d3.csv('../data/uberRidesPart2.csv',
 d3.csv('static/data/uberRidesPart2.csv',
